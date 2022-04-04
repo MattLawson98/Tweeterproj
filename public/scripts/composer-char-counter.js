4325ca6e99e174d5a6bd36054e8cd82a93ec03de
MattLawson98 <matthewlawson50@gmail.com>
2022-04-04 13:52:26 -0700

    This file creates a live character count within the text box area of tweeter!

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   console.log("Document Ready");
 
+  //Watches the live input within the textarea box!
   $('textarea').on('input', function(e) {
     let textlength = $(this).val().length;
     let lettersLeft = 140 - textlength;
 
+    //dom traversal to find the .counter
     let counter = $(this).parent().next('div').children('.counter');
     counter.text(lettersLeft);
 
